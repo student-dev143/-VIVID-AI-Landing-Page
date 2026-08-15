51604969a265104f80bffc334566770dac7edcbe
student-dev143 <raniarimsha5@gmail.com>
2026-08-16 00:37:37 +0500

    Add files via upload

---
+++ ai.js
@@ -0,0 +1,5 @@
+let menu = document.querySelector(".ri-menu-fill");
+let sideNav = document.querySelector(".side-nav")
+menu.addEventListener("click",()=>{
+    sideNav.classList.toggle("active")
+})
